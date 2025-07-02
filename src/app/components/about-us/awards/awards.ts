60e28cad7f8d90de3859cac63e20036c4fe75463
Full stack developer <fullstackdeveloper918@gmail.com>
2025-07-02 12:01:56 +0530

    updated code

--- src/app/components/about-us/awards/awards.ts
+++ src/app/components/about-us/awards/awards.ts
@@ -5,7 +5,7 @@ import { awardImages } from "../../../utils/Data";
   selector: 'app-awards',
   standalone: true,
   template: `
-      <div class="md:flex flex-wrap justify-center items-center gap-0 grid grid-cols-3 px-4 xl:px-10 gap-3">
+      <div class="md:flex flex-wrap xl:flex-nowrap justify-center items-center xl:gap-0 grid grid-cols-3 px-4 xl:px-10 gap-3">
     @for(image of awardImages;track image){
       <img [src]='image.imageLink' alt="Award 1"
       class="h-full md:h-35" />}
