05c304fff9baf232fc7158b5c02ca93ec4d2a31a
full stack developer <fullstackdeveloper918@gmail.com>
2025-07-02 21:14:35 +1000

    made the title dynamic based on the route

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'DCB-01';
+ constructor(
+    private titleService: Title,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let route = this.activatedRoute;
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      mergeMap(route => route.data)
+    ).subscribe(data => {
+      console.log('data', data)
+      const routeTitle = data['title'] ? ` ${data['title']} | DCB` : 'DCB';
+      this.titleService.setTitle(routeTitle);
+    });
+  }
 }
--- src/app/components/layout-routing-module.ts
+++ src/app/components/layout-routing-module.ts
@@ -6,7 +6,8 @@ import { NewHero } from './new-hero/new-hero/new-hero';
 const routes: Routes = [
   {
     path : '',
-    component : NewHero
+    component : NewHero,
+        data: { title: 'HOME' }
   },
   // {
   //   path : '',
@@ -17,66 +18,75 @@ const routes: Routes = [
     {
     path : 'Home',
     component : Layout,
-    loadChildren: () => import('../components/hero/hero-module').then((m) => m.HeroModule)
+    loadChildren: () => import('../components/hero/hero-module').then((m) => m.HeroModule),
+            data: { title: 'HOME' }
+
   },
   {
     path : 'about-dcb',
     component : Layout,
-    loadChildren : () => import('../components/about-us//about-us-module').then((m)=> m.AboutUsModule)
+    loadChildren : () => import('../components/about-us//about-us-module').then((m)=> m.AboutUsModule),
+    data: { title: 'About DCB' }
   },
   {
     path : 'projects',
     component : Layout,
-    loadChildren : () => import('../components/projects/projects-module').then((m)=>m.ProjectsModule)
+    loadChildren : () => import('../components/projects/projects-module').then((m)=>m.ProjectsModule),
+        data: { title: 'PROJECTS' }
   },
   {
     path : 'before-you-build',
     component : Layout,
-    loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule)
+    loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule),
+            data: { title: 'Before you build' }
   }, 
    {
     path : 'architects',
     component : Layout,
-    loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule)
+    loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule),
+                data: { title: 'Architects' }
   }, 
   {
     path : 'timing-to-consider',
       component : Layout,
-      loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule)
+      loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule),
+                      data: { title: 'Timing to consider' }
   },
   {
     path : 'expenses-to-consider',
       component : Layout,
-      loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule)
+      loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule),
+                            data: { title: 'expenses to consider' }
   },
   {
     path : 'housekeeping',
     component : Layout,
-    loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule)
+    loadChildren : () => import('../components/before-you-build/before-you-build-module').then((m)=> m.BeforeYouBuildModule),
+                          data: { title: 'Housekeeping' }
   },
   {
     path : '20-clubs',
     component : Layout,
-    loadChildren : () => import('../components/nahb/nahb-module').then((m)=>m.NahbModule)
+    loadChildren : () => import('../components/nahb/nahb-module').then((m)=>m.NahbModule),
+                          data: { title: '20 Clubs' }
   },
   {
     path : 'contact-us',
     component : Layout,
-    loadChildren : () => import('../components/contact-us/contact-us-module').then((m)=> m.ContactUsModule)
+    loadChildren : () => import('../components/contact-us/contact-us-module').then((m)=> m.ContactUsModule),
+                          data: { title: 'CONTACT US' }
   },
-  {
-    path : 'interior',
-    loadChildren : () => import('../components/interior/interior-module').then((m) => m.InteriorModule)
-  }, 
   {
     path : 'facts',
     component : Layout,
-    loadChildren : () => import('../components/facts/facts-module').then((m) => m.FactsModule)
+    loadChildren : () => import('../components/facts/facts-module').then((m) => m.FactsModule),
+                          data: { title: 'FACTS' }
   },
   {
     path : 'our-references',
     component : Layout,
-    loadChildren : () => import('../components/our-references/our-references-module').then((m) => m.OurReferencesModule)
+    loadChildren : () => import('../components/our-references/our-references-module').then((m) => m.OurReferencesModule),
+                          data: { title: 'Our References' }
   }
 ];
 
